0e75daf23798e21a1aad942c395005557e989d99
eric <ericstrong89@gmail.com>
2017-10-10 23:22:19 +0100

    blackened boxes

--- lab3/js/functions.js
+++ lab3/js/functions.js
@@ -17,6 +17,7 @@ function hide(target) {
 
 function show(target) {
     document.getElementById(target).style.display = '';
+    timer();
     tableCreate();
 }
 
@@ -32,20 +33,72 @@ function tableCreate(){
         var tr = tbl.insertRow();
         for(var j = 0; j < 3; j++){
           var td = tr.insertCell();
-          td.appendChild(document.createTextNode('COLOR'));
+          var color = getRandomColor();
+          td.appendChild(document.createTextNode(' '));
           td.style.border = '3px solid black';
-          td.style.backgroundColor = getRandomColor();
+          td.style.backgroundColor = color;
+          //td.onclick = displayColor(color);
+          td.onclick = blacken(td, color);
 
         }// end for
     }//end for
       var div = document.getElementById('gameArea2');
       div.appendChild(tbl);
+
 }
+
+// function displayColor(color){
+//
+//     return function(){
+//
+//         document.getElementById("color-is").innerText = color;
+//         document.getElementById("color-is").style.color = color;
+//
+//     }
+// }
+
+function blacken(td,color){
+  return function(){
+    td.style.backgroundColor = 'black';
+    document.getElementById("color-is").innerText = color;
+    document.getElementById("color-is").style.color = color;
+  }
+}
+
 
 function getRandomColor () {
 
   var myColors = ['blue', 'green', 'red', 'orange', 'purple', 'pink', 'gray'];
   var rand = myColors[Math.floor(Math.random() * myColors.length)];
   return rand;
+
+}
+
+function timer(){
+document.getElementById('count-up').style.display = '';
+  var min = 0;
+    var second = 00;
+    var zeroPlaceholder = 0;
+    var counterId = setInterval(function(){
+                      countUp();
+                    }, 1000);
+
+    function countUp () {
+        second++;
+        if(second == 59){
+          second = 00;
+          min = min + 1;
+        }
+        if(second == 10){
+            zeroPlaceholder = '';
+        }else
+        if(second == 00){
+            zeroPlaceholder = 0;
+        }
+
+      document.getElementById("count-up").innerText = min+':'+zeroPlaceholder+second;
+
+
+    }
 
 }
