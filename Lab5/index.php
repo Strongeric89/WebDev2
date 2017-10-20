ddcb11a41c7595db08f699b8606958c0a4981950
eric <ericstrong89@gmail.com>
2017-10-20 16:52:04 +0100

    lab5

--- Lab5/index.php
+++ Lab5/index.php
@@ -7,13 +7,23 @@
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <script src="js/jquery.js"></script>
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
 
   <style>
     /* Remove the navbar's default margin-bottom and rounded borders */
     .navbar {
       margin-bottom: 0;
       border-radius: 0;
+
     }
+    .carousel-inner > .item > img,
+.carousel-inner > .item > a > img {
+    width: 70%;
+    margin: auto;
+}
 
     /* Add a gray background color and some padding to the footer */
     footer {
@@ -52,6 +62,87 @@
   <div class="container text-center">
     <h1>My Portfolio solutions to the lab sheet</h1>
     <p>erics labs.</p>
+    <div class="container">
+  <h2>Simple Collapsible</h2>
+  <p>Click on the button to toggle between showing and hiding content.</p>
+  <button type="button" class="btn btn-info" data-toggle="collapse" data-target="#demo">Simple collapsible</button>
+  <div id="demo" class="collapse">
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+  </div>
+</div>
+  </div>
+</div>
+
+
+
+
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+
+  <!-- carousel-inner -->
+  <div class="container">
+  <br>
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+      <li data-target="#myCarousel" data-slide-to="3"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner" role="listbox">
+
+      <div class="item active">
+        <img src="images/sp3.jpg" alt="Chania" width="460" height="345">
+        <div class="carousel-caption">
+          <h3>Chania</h3>
+          <p>The atmosphere in Chania has a touch of Florence and Venice.</p>
+        </div>
+      </div>
+
+      <div class="item">
+        <img src="images/sp1.jpg" alt="Chania" width="460" height="345">
+        <div class="carousel-caption">
+          <h3>Chania</h3>
+          <p>The atmosphere in Chania has a touch of Florence and Venice.</p>
+        </div>
+      </div>
+
+      <div class="item">
+        <img src="images/sp2.jpg" alt="Flower" width="460" height="345">
+        <div class="carousel-caption">
+          <h3>Flowers</h3>
+          <p>Beautiful flowers in Kolymbari, Crete.</p>
+        </div>
+      </div>
+
+      <div class="item">
+        <img src="images/sp8.jpg" alt="Flower" width="460" height="345">
+        <div class="carousel-caption">
+          <h3>Flowers</h3>
+          <p>Beautiful flowers in Kolymbari, Crete.</p>
+        </div>
+      </div>
+
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
   </div>
 </div>
 
@@ -60,28 +151,92 @@
   <div class="row">
     <div class="col-sm-3">
       <p>Some text..</p>
-      <img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">
+      <img src="images/sp1.jpg"  class="img-circle" style="width:100%" alt="Image">
     </div>
     <div class="col-sm-3">
       <p>Some text..</p>
-      <img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">
+      <img src="images/sp2.jpg" class="img-circle" style="width:100%" alt="Image">
     </div>
     <div class="col-sm-3">
       <p>Some text..</p>
-      <img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">
+      <img src="images/sp3.jpg" class="img-circle" style="width:100%" alt="Image">
     </div>
     <div class="col-sm-3">
       <p>Some text..</p>
-      <img src="https://placehold.it/150x80?text=IMAGE" class="rounded-circle" style="width:100%" alt="Image">
+      <img src="images/sp8.jpg" class="img-circle" style="width:100%" alt="Image">
     </div>
   </div>
 </div><br>
 
 </div><br><br>
 
+<div class="container">
+  <h2>Table</h2>
+  <p>The .table-striped class adds zebra-striping to any table row within tbody (not available in IE8):</p>
+  <table class="table table-striped">
+    <thead>
+      <tr>
+        <th>#</th>
+        <th>Firstname</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>1</td>
+        <td>Anna</td>
+      </tr>
+      <tr>
+        <td>2</td>
+        <td>Debbie</td>
+      </tr>
+      <tr>
+        <td>3</td>
+        <td>John</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+
+
+<div class="container">
+  <div class="row">
+    <div class="col-sm-12" style="background-color:green;">sm hello</div>
+  </div>
+  <div class="row">
+    <div class="col-sm-4" style="background-color:blue;">sm span 4</div>
+    <div class="col-sm-4" style="background-color:yellow;">sm span 4</div>
+    <div class="col-sm-4" style="background-color:red;">sm span 4</div>
+  </div>
+  <div class="row">
+
+  </div>
+</div>
+
+<br>
+<div class="container">
+  <div class="row">
+    <div class="col-xs-12" style="background-color:green;">xs hello</div>
+  </div>
+  <div class="row">
+    <div class="col-xs-4" style="background-color:blue;">xs span 4</div>
+    <div class="col-xs-4" style="background-color:yellow;"> xs span 4</div>
+    <div class="col-xs-4" style="background-color:red;">xs span 4</div>
+  </div>
+  <div class="row">
+
+  </div>
+</div>
+
+<br>
+
+
+
+
+
 <footer class="container-fluid text-center">
   <p>Footer Text</p>
 </footer>
+
 
 </body>
 </html>
