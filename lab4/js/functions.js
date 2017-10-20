0a1235fcc1c11470153026eaa01f363855f6bc06
eric <ericstrong89@gmail.com>
2017-10-20 17:26:56 +0100

    lab4

--- lab4/js/functions.js
+++ lab4/js/functions.js
@@ -1,9 +1,11 @@
+    validDOB();
 function check_form(){
     var fields = ["name_text", "email_text", "day_text", "month_text", "year_text", "select_gender", "addr1_text", "addr2_text", "city_text","zip_text",];
     var fieldname;
 
     lettersOnly();
     validEmail();
+
 
     for(var i = 0; i < fields.length; i++){
       fieldname = fields[i];
@@ -43,12 +45,58 @@ function validDOB(){
   var pattern =/^([0-9]{2})-([0-9]{2})-([0-9]{4})$/;
 
   var day = document.forms["personal_info"]["day_text"].value;
-    var day = document.forms["personal_info"]["month_text"].value;
-      var day = document.forms["personal_info"]["year_text"].value;
+    var month = document.forms["personal_info"]["month_text"].value;
+      var year = document.forms["personal_info"]["year_text"].value;
 
- if(!inputtxt.match(email)){
-   alert("not a Date of Birth");
-       //document.forms["personal_info"]["email_text"].focus();
-     }
+      if(day < 1 && day > 31){
+        alert('not a valid day');
+          document.write('invalid day');
+           document.forms["personal_info"]["day_text"].focus();
+      }
 
+      if(month < 1 && month > 12){
+        alert('not a valid month');
+          document.write('invalid month');
+           document.forms["personal_info"]["month_text"].focus();
+      }
+
+      var now = new Date();
+      var entereddate = day+ ' '+ month + ' '+ year;
+      document.write(now);
+      document.write(entereddate);
+
+
+
+}
+
+
+function isleap()
+{
+ var yr=document.getElementById("year_text").value;
+ if ((parseInt(yr)%4) == 0)
+ {
+  if (parseInt(yr)%100 == 0)
+  {
+    if (parseInt(yr)%400 != 0)
+    {
+    alert("Not Leap");
+    return "false";
+    }
+    if (parseInt(yr)%400 == 0)
+    {
+    alert("Leap");
+    return "true";
+    }
+  }
+  if (parseInt(yr)%100 != 0)
+  {
+    alert("Leap");
+    return "true";
+  }
+ }
+ if ((parseInt(yr)%4) != 0)
+ {
+    alert("Not Leap");
+    return "false";
+ }
 }
