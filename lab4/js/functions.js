0e49871c48fe02f065116b3e3da340aaa6dd60f1
eric <ericstrong89@gmail.com>
2017-11-07 18:09:37 +0000

    lab 4

--- lab4/js/functions.js
+++ lab4/js/functions.js
@@ -1,9 +1,7 @@
-var send = false;
-function validate() {
 
-  // var x = document.getElementById('heading');
-  // x.innerHTML = "TRUE";
-  send = true;
+function validate(flag) {
+
+  flag = false;
   //get all the elements first
   var name = document.myform.name.value;
   var email = document.myform.email.value;
@@ -30,111 +28,148 @@ function validate() {
   if (name.length == 0) {
 
 
-    document.myform.name.focus();
+
     alert('you must enter a name: Eric Strong');
-    send = false;
+      document.myform.name.focus();
+      return false;
+    flag = false;
 
 
   }
   if (email.length == 0) {
 
-    document.myform.email.focus();
+
+    //document.getElementById("email").focus();
       alert('you must enter an email: example@domain.com');
-          send = false;
-  }
-
-  if (day.length == 0) {
-
-    document.myform.day.focus();
-      alert('you must enter a day: 1-31');
-          send = false;
-  }
-  if (month.length == 0) {
-
-    document.myform.month.focus();
-      alert('you must enter a month: 1-12');
-          send = false;
+                flag = false;
+                  return false;
 
   }
-  if (year.length == 0) {
 
-    document.myform.year.focus();
-      alert('you must enter a year : yyyy');
-          send = false;
-
-  }
+  // if (day.length == 0) {
+  //
+  //   document.myform.day.focus();
+  //     alert('you must enter a day: 1-31');
+  //
+  //             flag = false;
+  // }
+  // if (month.length == 0) {
+  //
+  //   document.myform.month.focus();
+  //     alert('you must enter a month: 1-12');
+  //
+  //             flag = false;
+  //
+  // }
+  // if (year.length == 0) {
+  //
+  //   document.myform.year.focus();
+  //     alert('you must enter a year : yyyy');
+  //
+  //             flag = false;
+  //
+  // }
   if (gender.length == 0) {
 
-    document.myform.gender.focus();
+
       alert('you must enter a gender: select one');
-          send = false;
+      document.myform.gender.focus();
+              flag = false;
+                return false;
 
   }
   if (addr1.length == 0) {
 
-    document.myform.addr1.focus();
+
       alert('you must enter an address 1: number street');
-          send = false;
+      document.myform.addr1.focus();
+
+                flag = false;
+                  return false;
 
   }
 
   if (addr2.length == 0) {
 
-    document.myform.addr2.focus();
+
       alert('you must enter an address 2: road place');
-          send = false;
+      document.myform.addr2.focus();
+
+              flag = false;
+                return false;
 
   }
 
   if (city.length == 0) {
 
-    document.myform.city.focus();
+
     alert('you must enter a city: city name');
-        send = false;
+    document.myform.city.focus();
+
+            flag = false;
+              return false;
 
   }
 
   if (zip.length == 0) {
 
-    document.myform.zip.focus();
+
       alert('you must enter a zip: example Db 1');
-          send = false;
+      document.myform.zip.focus();
+
+              flag = false;
+                return false;
 
   }
 
 
+
   //b. check if name has lettes only
-  lettersOnly(name, "name");
+   flag = lettersOnly(name, "name");
 
   //c. email is a valid email
-  validEmail(email);
+  flag = validEmail(email);
 
   //d. valid date and not in the future
 
 
-  validDate(day,month,year);
+//  flag = validDate(day,month,year);
 
   //e. checking gender is not left blank
-  validateGender(gender);
+  flag = validateGender(gender);
 
   //f. validating City has letters only
-  lettersOnly(city, "City");
+  flag = lettersOnly(city, "City");
 
   //g. Zip code has at least one letter followed by a white space, followed by at least one digit
-  validateZip(zip);
+  flag = validateZip(zip);
+
+  if(flag == true){
+      alert('Thank you for your details');
+      var x = document.getElementById("container");
+      x.style.display = "none";
+
+      return true;
+
+
+  }else{
+    return false;
+  }
 
 
 
 } //end validate
 
 function lettersOnly(element, field) {
-  var letters = /^[A-Za-z]+$/;
+  var letters = /^[a-zA-Z\s]*$/;
 
   if (!element.match(letters)) {
     alert(field + " Must contain letters only!");
-        send = false;
+    document.myform.name.focus();
+        return false;
 
   }
+  return true;
 
 } //end nameLettersOnly
 
@@ -144,9 +179,11 @@ function validEmail(element) {
 
   if (!element.match(email)) {
     alert("not a valid email");
-        send = false;
+    document.myform.email.focus();
+      return false;
 
   }
+    return true;
 
 }//end validEmail
 
@@ -157,29 +194,28 @@ function validDate(day,month,year){
   var yearNow = now.getFullYear();
   var monthNow = now.getMonth();
 
-
-
-
-
   if(year > yearNow && month > monthNow ){
     alert("not a valid date :" + givenDate);
-        send = false;
+            return false;
   }
 
   if(year > yearNow){
     alert("not a valid year :" + year);
-        send = false;
+
+            return false;
 
   }
 
   if(day > 31 || day < 1 ){
       alert("not a valid day :" + day);
-          send = false;
+
+              return false;
   }
 
   if(month > 12 || month < 1){
     alert("not a valid month :" + month);
-        send = false;
+
+            return false;
   }
 
   if(month == 2){
@@ -187,9 +223,10 @@ function validDate(day,month,year){
       if(!isLeapYear){
           if(day > 28){
               alert("not a valid day for feb  :" + day);
-                  send = false;
+                      return false;
           }
       }
+        return true;
   }
 
 
@@ -215,13 +252,15 @@ function isleap(yr)
     }
   }
 }
+  return true;
 }
 
 function validateGender(gender){
   if(gender == "X"){
     alert("you cannot leave gender blank");
-        send = false;
+            return false;
   }
+    return true;
 }
 
 function validateZip(element){
@@ -229,7 +268,9 @@ function validateZip(element){
 
   if (!element.match(zip)) {
     alert("not a valid zip code");
-        send = false;
+            return false;
 
   }
+    return true;
+
 }
